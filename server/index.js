bcd95084b40f74d9b3ab1e4cfe4d4aef1b3e3efb
Steve Farkas <steve@Steves-MacBook-Air.local>
2022-02-12 17:09:42 -0500

    initiating express react app

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// In order for Express to serve up all static web files, we need to provide some middleware with the express.static built-in middleware function.
+app.use(express.static("public"));
+
+app.listen(5000, () => {
+  console.log("App listening on port 5000");
+});
