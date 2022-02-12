42e5a23201e774c6c8e778ebae41af41e08bb1fb
Steve Farkas <steve@Steves-MacBook-Air.local>
2022-02-12 17:40:20 -0500

    app configured to make production build of react app and start node express server on the start command from root. Also set up client-side routing by making the express server always route any request to index.html

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
 app.listen(5000, () => {
   console.log("App listening on port 5000");
 });
