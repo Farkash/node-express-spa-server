efc9a7bb822212b1b42bd03901b9ecb417894e06
Steve Farkas <steve@Steves-MacBook-Air.local>
2022-02-12 17:28:43 -0500

    configured to build and run react app

--- react-app/server/index.js
+++ react-app/server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 
 // In order for Express to serve up all static web files, we need to provide some middleware with the express.static built-in middleware function.
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
 
 app.listen(5000, () => {
